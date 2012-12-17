778ac33d38ba85d3fcb9557b3099c1db97c3e160
erroroccured <craigpayne1985@googlemail.com>
2012-12-17 11:39:43 +0000

    first commit

---
+++ khronos.js
@@ -0,0 +1,94 @@
+﻿Date.prototype.today = function () {
+    var now = new Date();
+    return new Date(now.year(), now.month(), now.day(), 0, 0, 0, 0);
+};
+
+Date.prototype.addMilliseconds = function (amount) { ///<summary>Adds milliseconds to a date object</summary><param name="amount">An amount of milliseconds</param>
+    this.setMilliseconds(this.getMilliseconds() + amount);
+    return this;
+};
+Date.prototype.addSeconds = function (amount) { ///<summary>Adds seconds to a date object</summary><param name="amount">An amount of seconds</param>
+    this.setSeconds(this.getSeconds() + amount);
+    return this;
+};
+Date.prototype.addMinutes = function (amount) { ///<summary>Adds minutes to a date object</summary><param name="amount">An amount of minutes</param>
+    this.setMinutes(this.getMinutes() + amount);
+    return this;
+};
+Date.prototype.addHours = function (amount) { ///<summary>Adds hours to a date object</summary><param name="amount">An amount of hours</param>
+    this.setHours(this.getHours() + amount);
+    return this;
+};
+Date.prototype.addDays = function (amount) { ///<summary>Adds days to a date object</summary><param name="amount">An amount of days</param>
+    this.setHours(this.getHours() + (amount * 24));
+    return this;
+};
+Date.prototype.addWeeks = function (amount) { ///<summary>Adds weeks to a date object</summary><param name="amount">An amount of weeks</param>
+    this.addDays(amount * 7);
+    return this;
+};
+Date.prototype.addMonths = function (amount) { ///<summary>Adds months to a date object</summary><param name="amount">An amount of months</param>
+    this.setMonth(this.getMonth() + amount);
+    return this;
+};
+Date.prototype.addYears = function (amount) { ///<summary>Adds years to a date object</summary><param name="amount">An amount of years</param>
+    this.setFullYear(this.getFullYear() + amount);
+    return this;
+};
+Date.prototype.isAfter = function (date) {///<summary>Is the specified date after this date?</summary><param name="date">A specified date</param>
+    return this.getTime() > date.getTime();
+};
+Date.prototype.isBefore = function (date) {///<summary>Is the specified date before this date?</summary><param name="date">A specified date</param>
+    return this.getTime() < date.getTime();
+};
+
+Date.prototype.add = function (years, months, days, hours, minutes, seconds, milliseconds) {
+    ///<summary>Adds to a date</summary>
+    this.addYears(years == null ? 0 : years);
+    this.addMonths(months == null ? 0 : months);
+    this.addDays(days == null ? 0 : days);
+    this.addHours(hours == null ? 0 : hours);
+    this.addSeconds(seconds == null ? 0 : seconds);
+    this.addMilliseconds(milliseconds == null ? 0 : milliseconds);
+    return this;
+};
+
+Date.prototype.milliseconds = function () {
+    return this.getMilliseconds();
+};
+Date.prototype.second = function () {
+    return this.getSeconds();
+};
+Date.prototype.minute = function () {
+    return this.getMinutes();
+};
+Date.prototype.hour = function () {
+    return this.getHours();
+};
+Date.prototype.day = function () {
+    return this.getUTCDate();
+};
+Date.prototype.month = function () {
+    return this.getUTCMonth();
+};
+Date.prototype.year = function () {
+    return this.getUTCFullYear();
+};
+
+
+Date.prototype.dayOfWeek = function () {
+    switch (this.getDay()) {
+        case 0: return "Sunday";
+        case 1: return "Monday";
+        case 2: return "Tuesday";
+        case 3: return "Wednesday";
+        case 4: return "Thursday";
+        case 5: return "Friday";
+        case 6: return "Saturday";
+    }
+};
+
+//Specialist methods
+Date.prototype.isLeapYear = function () {
+    return new Date(this.year(), 1, 29).getMonth() == 1
+};
---
+++ khronos.tests.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="khronos.js" />
+/// <reference path="jasmine/jasmine.js" />
+var second = 1000;
+var minute = 60000;
+var hour = minute * 60;
+var day = hour * 24;
+
+var now = new Date();
+describe("When testing Khronos.js, using the current Date/Time" + now, function () {
+
+    it("The current date, should equal the current date", function () {
+        expect(now).toEqual(now);
+    });
+
+    it("The addDays method should work", function () {
+        var result1 = new Date(now.getTime() + (day * 2));
+        var result2 = new Date(now.getTime() - (day * 2));
+
+        //scenario 1
+        var date = new Date(now);
+        date.addDays(2);
+        expect(date.getDate()).toEqual(result1.getDate());
+        
+        //scenario 2
+        expect(new Date(now).addDays(2).getDate()).toEqual(result1.getDate());
+
+        //scenario 3
+        expect(new Date(now).addDays(-2).getDate()).toEqual(result2.getDate());
+    });
+
+    it("Is leap year should detect a leap year", function () {
+        expect(new Date(2012, 01, 01, 01, 01, 01, 01).isLeapYear()).toEqual(true);
+        expect(new Date(2011, 01, 01, 01, 01, 01, 01).isLeapYear()).toEqual(false);
+        expect(new Date(2010, 01, 01, 01, 01, 01, 01).isLeapYear()).toEqual(false);
+        expect(new Date(2009, 01, 01, 01, 01, 01, 01).isLeapYear()).toEqual(false);
+
+    });
+
+    it("Is should format date correctly" + now.toString("mmYY"), function () {
+        
+    });
+});
+
